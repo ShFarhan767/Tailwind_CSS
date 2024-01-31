009d00c1e78b92ead606184c7b42a90a73609588
ShFarhan767 <farhan@doofazit.com>
2024-01-31 12:33:58 +0600

    tailwind

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Slider from './components/Slider.vue';
+import Card from './components/Card.vue';
+import Tab from './components/Tab.vue';
+import Jumb from './components/Jumb.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <Navbar />
+  <Slider />
+  <Card />
+  <Jumb />
+  <Tab />
+  <Footer />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,117 @@
+<script setup>
+const carouselElement = document.getElementById('carousel-example');
+
+const items = [
+    {
+        position: 0,
+        el: document.getElementById('carousel-item-1'),
+    },
+    {
+        position: 1,
+        el: document.getElementById('carousel-item-2'),
+    },
+    {
+        position: 2,
+        el: document.getElementById('carousel-item-3'),
+    },
+    {
+        position: 3,
+        el: document.getElementById('carousel-item-4'),
+    },
+];
+
+// options with default values
+const options = {
+    defaultPosition: 1,
+    interval: 3000,
+
+    indicators: {
+        activeClasses: 'bg-white dark:bg-gray-800',
+        inactiveClasses:
+            'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
+        items: [
+            {
+                position: 0,
+                el: document.getElementById('carousel-indicator-1'),
+            },
+            {
+                position: 1,
+                el: document.getElementById('carousel-indicator-2'),
+            },
+            {
+                position: 2,
+                el: document.getElementById('carousel-indicator-3'),
+            },
+            {
+                position: 3,
+                el: document.getElementById('carousel-indicator-4'),
+            },
+        ],
+    },
+
+    // callback functions
+    onNext: () => {
+        console.log('next slider item is shown');
+    },
+    onPrev: () => {
+        console.log('previous slider item is shown');
+    },
+    onChange: () => {
+        console.log('new slider item has been shown');
+    },
+};
+
+// instance options object
+const instanceOptions = {
+  id: 'carousel-example',
+  override: true
+};
+</script>
+
+<template>
+
+    <div id="animation-carousel" class="relative w-full h-max" data-carousel="static">
+        <!-- Carousel wrapper -->
+        <div class="relative h-max overflow-hidden md:h-96">
+            <!-- Item 1 -->
+            <div class="hidden duration-200 ease-linear" data-carousel-item>
+                <img src="https://static.toiimg.com/photo/43425856.cms" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+            <!-- Item 2 -->
+            <div class="hidden duration-200 ease-linear" data-carousel-item>
+                <img src="https://static.toiimg.com/photo/43425856.cms" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+            <!-- Item 3 -->
+            <div class="hidden duration-200 ease-linear" data-carousel-item="active">
+                <img src="https://static.toiimg.com/photo/43425856.cms" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+            <!-- Item 4 -->
+            <div class="hidden duration-200 ease-linear" data-carousel-item>
+                <img src="https://static.toiimg.com/photo/43425856.cms" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+            <!-- Item 5 -->
+            <div class="hidden duration-200 ease-linear" data-carousel-item>
+                <img src="https://static.toiimg.com/photo/43425856.cms" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+        </div>
+        <!-- Slider controls -->
+        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                </svg>
+                <span class="sr-only">Previous</span>
+            </span>
+        </button>
+        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none focus:ring-transparent" data-carousel-next>
+            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                </svg>
+                <span class="sr-only">Next</span>
+            </span>
+        </button>
+    </div>
+
+
+</template>
